222b4fad2f664ab1d579aff7594195dc344fa105
Angelo <matheussavioti@gmail.com>
2021-01-02 00:28:35 -0300

    added GetContrastingColor

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace YoukaiFox.UnityExtensions
+{
+    public static class ColorExtensions 
+    {
+        // Origin: Stack Overflow
+        /// <summary>
+        /// Decides which color from black or white which
+        /// contrasts better with this color <paramref name="self"/>.
+        /// </summary>
+        public static Color GetContrastingColor(this Color self)
+        {
+            // Math taken from one of the replies to
+            // http://stackoverflow.com/questions/2241447/make-foregroundcolor-black-or-white-depending-on-background
+            float colorProduct = self.r * self.r * 0.241f + self.g * self.g * 0.691f + self.b * self.b * 0.068f;
+
+            if (Mathf.Sqrt(colorProduct) > 128f)
+                return Color.black;
+
+            return Color.white;
+        }
+    }
+}
