2680e44585f6e83eb0d80541b196e1044e4c45c0
Angelo <matheussavioti@gmail.com>
2021-01-12 02:02:03 -0300

    improved documentation

--- Extensions/ColorExtensions.cs
+++ Extensions/ColorExtensions.cs
@@ -9,10 +9,9 @@ namespace YoukaiFox.UnityExtensions
         /// Decides which color from black or white which
         /// contrasts better with this color <paramref name="self"/>.
         /// </summary>
+        /// </<seealso cref="http://stackoverflow.com/questions/2241447/make-foregroundcolor-black-or-white-depending-on-background"/>
         public static Color GetContrastingColor(this Color self)
         {
-            // Math taken from one of the replies to
-            // http://stackoverflow.com/questions/2241447/make-foregroundcolor-black-or-white-depending-on-background
             float colorProduct = self.r * self.r * 0.241f + self.g * self.g * 0.691f + self.b * self.b * 0.068f;
 
             if (Mathf.Sqrt(colorProduct) > 128f)
