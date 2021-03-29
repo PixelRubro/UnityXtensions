1c8dbbe94900d16ab60201a7e951d275991edcb7
Angelo <matheussavioti@gmail.com>
2021-03-28 22:39:52 -0300

    added add multiple and remove multiple in ICollection

---
+++ Extensions/ICollectionExtensions.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+
+namespace YoukaiFox.UnityExtensions
+{
+    public static class ICollectionExtensions 
+    {
+        /// <summary>
+        /// Adds each one of the items in <paramref name="items"/>.
+        /// </summary>
+        public static void Add<T>(this ICollection self, params T[] items)
+        {
+            foreach (var item in items)
+            {
+                self.Add(item);
+            }
+        }
+
+        /// <summary>
+        /// Removes each one of the items in <paramref name="items"/>
+        /// if they are found.
+        /// </summary>
+        /// <returns>True if all provided items were removed.</returns>
+        public static bool Remove<T>(this ICollection self, params T[] items)
+        {
+            var removedAll = true;
+
+            foreach (var item in items)
+            {
+                var removed = self.Remove(item);
+
+                if (!removed)
+                    removedAll = false;
+            }
+
+            return removedAll;
+        } 
+    }
+}
