0e957366993e45f4d3f62acbfedebf90136b30d8
msavioti <angelosavioti@gmail.com>
2021-12-11 17:03:13 -0300

    added contains bounds for bounds extensions

---
+++ Extensions/BoundsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YoukaiFox.UnityExtensions
+{
+    public static class BoundsExtensions
+    {
+        /// <summary>
+        /// Checks if <paramref name="bounds"/> is contained by the bounds <paramref name="self"/>.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="bounds"></param>
+        public static bool ContainsBounds(this Bounds self, Bounds bounds)
+        {
+            return self.Contains(bounds.min) && self.Contains(bounds.max);
+        }
+    }
+}
