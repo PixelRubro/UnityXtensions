8f158b5872889a2c4090f29fee81eb0886748131
msavioti <angelosavioti@gmail.com>
2021-12-15 21:33:20 -0300

    added vertical bounds contain check

--- Extensions/BoundsExtensions.cs
+++ Extensions/BoundsExtensions.cs
@@ -15,5 +15,27 @@ namespace YoukaiFox.UnityExtensions
         {
             return self.Contains(bounds.min) && self.Contains(bounds.max);
         }
+
+        /// <summary>
+        /// Checks if <paramref name="bounds"/> is contained by the bounds
+        /// <paramref name="self"/> horizontally.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="bounds"></param>
+        public static bool ContainsHorizontalBounds(this Bounds self, Bounds bounds)
+        {
+            return self.min.x < bounds.min.x && self.max.x > bounds.max.x;
+        }
+
+        /// <summary>
+        /// Checks if <paramref name="bounds"/> is contained by the bounds
+        /// <paramref name="self"/> vertically.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="bounds"></param>
+        public static bool ContainsVerticalBounds(this Bounds self, Bounds bounds)
+        {
+            return self.min.y < bounds.min.y && self.max.y > bounds.max.y;
+        }
     }
 }
